c20dd5dacf8a3ce05fe1706e819ec32866aa3fad
bayramichAB <bayramich.a@gmail.com>
2024-01-30 11:40:02 +0300

    I added Repository User Interfaces and Classes

---
+++ Repository/AuthorRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Models;
+using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class AuthorRepository:RepositoryBase<Author>,IAuthorRepository
+    {
+        public AuthorRepository(RepositoryContext repositoryContext):base(repositoryContext)
+        {
+
+        }
+    }
+}
---
+++ Repository/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Models;
+using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class CategoryRepository:RepositoryBase<Category>,ICategoryRepository
+    {
+        public CategoryRepository(RepositoryContext repositoryContext):base(repositoryContext)
+        {
+
+        }
+    }
+}
