bfc8849931af6a0413ebe48f4dd1a7d89e79d26a
bayramichAB <bayramich.a@gmail.com>
2024-01-30 18:14:56 +0300

    I added CategoriesController

---
+++ TheBookshelf.Presentation/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBookshelf.Presentation.Controllers
+{
+    [Route("api/categories")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {   
+    }
+}
--- TheBookshelf/Program.cs
+++ TheBookshelf/Program.cs
@@ -16,7 +16,10 @@ builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 
-builder.Services.AddControllers();
+/*Without this code, our API wouldn’t work, and wouldn’t know where to route incoming requests. 
+ *But now, our app will find all of the controllers inside of the Presentation project and 
+ *configure them with the framework.*/
+builder.Services.AddControllers().AddApplicationPart(typeof(TheBookshelf.Presentation.AssemblyReference).Assembly);
 
 var app = builder.Build();
 
